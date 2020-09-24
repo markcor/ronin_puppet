46d920d65daac650e73277ffdcdd2a450d0ab9a3
markcor <mcornmesser@mozilla.com>
2020-09-24 08:52:43 -0700

    only use very silent imagemagick isntallation flag

--- modules/win_packages/manifests/imagemagick.pp
+++ modules/win_packages/manifests/imagemagick.pp
@@ -8,7 +8,7 @@ class win_packages::imagemagick (
 
     win_packages::win_exe_pkg {'imagemagick':
         pkg                    => "ImageMagick-${version}.exe",
-        install_options_string => '/SP- /VERYSILENT',
+        install_options_string => '/VERYSILENT',
         creates                => "${facts['custom_win_programfiles']}\\ImageMagick-${version}\\magick.exe",
     }
 }
