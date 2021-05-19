eb3d31c423f2eb8875b8212dcd73163224c3acd9
markcor <mcornmesser@mozilla.com>
2021-05-19 11:06:17 -0700

    debugging

--- modules/win_mozilla_build/manifests/init.pp
+++ modules/win_mozilla_build/manifests/init.pp
@@ -27,16 +27,16 @@ class win_mozilla_build (
     if $::operatingsystem == 'Windows' {
         include win_mozilla_build::install
         include win_mozilla_build::hg_install
-        include win_mozilla_build::hg_files
-        include win_mozilla_build::install_py3_certi
-        include win_mozilla_build::tooltool
-        include win_mozilla_build::modifications
-        include win_mozilla_build::set_registry_priority
-        include win_mozilla_build::virtualenv_support
-        include win_mozilla_build::pip
-        include win_mozilla_build::grant_symlnk_access
-        include win_mozilla_build::zstandard
-        include win_mozilla_build::install_psutil
+        #include win_mozilla_build::hg_files
+        #include win_mozilla_build::install_py3_certi
+        #include win_mozilla_build::tooltool
+        #include win_mozilla_build::modifications
+        #include win_mozilla_build::set_registry_priority
+        #include win_mozilla_build::virtualenv_support
+        #include win_mozilla_build::pip
+        #include win_mozilla_build::grant_symlnk_access
+        #include win_mozilla_build::zstandard
+        #include win_mozilla_build::install_psutil
     } else {
         fail("${module_name} does not support ${::operatingsystem}")
     }
