244c2227a912b6171b56e2ee4ecd69d99ee3d08a
markcor <mcornmesser@mozilla.com>
2021-05-19 12:04:36 -0700

    testing python 3 component install

--- modules/win_mozilla_build/manifests/custom_win32_python_3_7_2.pp
+++ modules/win_mozilla_build/manifests/custom_win32_python_3_7_2.pp
@@ -7,14 +7,94 @@ class win_mozilla_build::custom_win32_python_3_7_2 (
     String $install_path
 ) {
 
-  win_mozilla_build::win32_python_3_7_2_component { 'python_3_7_3_win32_core' :
+  win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Development Libraries (32-bit)' :
     source       => $source,
     install_path =>  $install_path,
     pkg          =>  'core',
   }
-    win_mozilla_build::win32_python_3_7_2_component { 'python_3_7_3_win32_core_d' :
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Development Libraries (32-bit debug)' :
         source       => $source,
         install_path => $install_path,
         pkg          =>  'core_d',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Documentation (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'doc',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Executables (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'exe',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Executables (32-bit debug)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'exe_d',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Executables (32-bit symbols)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'exe_pdb',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python Launcher' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'launcher',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Standard Library (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'lib',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Standard Library (32-bit debug)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'lib_d',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Standard Library (32-bit symbols)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'lib_pdb',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Add to Path (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'path',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 pip Bootstrap (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'pip',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Tcl/Tk Support (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'tcltk_pdb',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Test Suite (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'test_d',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Test Suite (32-bit debug)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'test_d',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Test Suite (32-bit symbols)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'test_pdb',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 Utility Scripts (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'tools',
+    }
+    win_mozilla_build::win32_python_3_7_2_component { 'Python 3.7.3 C Runtime Library (32-bit)' :
+        source       => $source,
+        install_path => $install_path,
+        pkg          =>  'ucrt',
     }
 }
