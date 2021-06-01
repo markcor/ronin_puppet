63854aa19f62153059fd5079f60aef5ae5198d9d
markcor <mcornmesser@mozilla.com>
2021-06-01 15:29:56 -0700

    debug

--- modules/roles_profiles/manifests/profiles/windows_worker_runner.pp
+++ modules/roles_profiles/manifests/profiles/windows_worker_runner.pp
@@ -49,6 +49,9 @@ class roles_profiles::profiles::windows_worker_runner {
                     $worker_pool_id        = lookup('win-worker.taskcluster.worker_pool_id')
                     $worker_group          = lookup('win-worker.taskcluster.worker_group')
                     $worker_id             = $facts['networking']['hostname']
+                    file { "${generic_worker_dir}\\standalone_token.txt}":
+                        content => " standalone  my tok is ${access_token}",
+                    }
                 }
                 'static': {
                     $taskcluster_root_url  = lookup('windows.taskcluster.root_url')
@@ -57,6 +60,9 @@ class roles_profiles::profiles::windows_worker_runner {
                     $worker_pool_id        = lookup('win-worker.taskcluster.worker_pool_id')
                     $worker_group          = lookup('win-worker.taskcluster.worker_group')
                     $worker_id             = $facts['networking']['hostname']
+                    file { "${generic_worker_dir}\\static_token.txt}":
+                        content => " static  my tok is ${access_token}",
+                    }
                 }
                 default: {
                     $taskcluster_root_url  = undef
@@ -65,6 +71,9 @@ class roles_profiles::profiles::windows_worker_runner {
                     $worker_pool_id        = undef
                     $worker_group          = undef
                     $worker_id             = undef
+                    file { "${generic_worker_dir}\\NOT_token.txt}":
+                        content => " NOT  my tok is ${access_token}",
+                    }
                 }
             }
 
